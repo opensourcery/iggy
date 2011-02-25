a0743095acc0969054e5302bb87c5cd208ee9438
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2011-02-24 18:07:40 -0800

    Pull out some colors into designkit template.

--- templates/designkit.tpl.php
+++ templates/designkit.tpl.php
@@ -1,7 +1,23 @@
 <style type="text/css">
 
 #navigation {
-  background-color: <?php print $background; ?>
+  background-color: <?php print $primary; ?>;
+}
+
+.calendar-calendar td.empty {
+  background-color: #E1E1E1;
+}
+.calendar-calendar table td {
+  background-color: #CBCBCB;
+}
+.calendar-calendar tr td.today,
+.calendar-calendar tr.odd td.today,
+.calendar-calendar tr.even td.today {
+  background-color: <?php print $primary; ?>;
+}
+.calendar-calendar table th.days {
+  color: #B1B1B1;
+  background-color: <?php print $primary; ?>;
 }
 
 </style>
